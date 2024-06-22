f7722bd81bf63df1814f9441f894351f45b14585
Omaan Mallick <omaanmallick786@gmail.com>
2024-06-23 03:04:26 +0530

    Initial Commit

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 export default function home() {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text>Home</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
